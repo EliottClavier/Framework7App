eee4d26a9afe7e0909c45908f5d56818f61716ba
Florent <florent.chabin@students.campus.academy>
2021-11-23 14:25:00 +0100

    Ajout de content sur le profil

--- www/js/routes.js
+++ www/js/routes.js
@@ -18,53 +18,18 @@ var routes = [
   },
   {
     path: '/profile',
-    url: './pages/profile.html',
-  },
-  {
-    path: '/request-and-load/user/:userId/',
     async: function ({ router, to, resolve }) {
       // App instance
       var app = router.app;
-
       // Show Preloader
       app.preloader.show();
 
-      // User ID from request
-      var userId = to.params.userId;
+      // Hide Preloader
+      app.preloader.hide();
 
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            componentUrl: './pages/request-and-load.html',
-          },
-          {
-            props: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
+      resolve({
+          componentUrl: "./pages/profile.html",
+      });
     },
   },
   // Default route (404 page). MUST BE THE LAST
