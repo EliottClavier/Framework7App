8f5c5585525c39f2d1be236764d89850401a1b9b
Clavier Eliott <clavier.eliott.el@gmail.com>
2021-11-24 14:20:00 +0100

    Réorganisation requetes et fichier recipe-detail

--- www/js/routes.js
+++ www/js/routes.js
@@ -28,28 +28,13 @@ var routes = [
   },
   {
     path: '/recipe/:recipeId',
-    async: function ({ router, to, resolve }) {
-      // App instance
-      var app = router.app;
-      // Show Preloader
-      app.preloader.show();
-
-      // Hide Preloader
-      app.preloader.hide();
-
-      // Requête GET
-      app.request.get(
-        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${to.params.recipeId}`
-      )
-        .then(r => {
-          let recipe = JSON.parse(r.data).meals[0];
-          resolve({
-              componentUrl: "./pages/recipe-detail.html",},
-            {
-              props: {
-                recipe: recipe
-              }
-            });
+    async: async function ({ router, to, resolve }) {
+      resolve({
+          componentUrl: "./pages/recipe-detail.html",},
+        {
+          props: {
+            recipe: await getRecipeById(to.params.recipeId),
+          }
         });
     }
   },
