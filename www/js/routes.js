156deef6a3a9f4a13a7d4cbf9dc17d2d63520d0e
Clavier Eliott <clavier.eliott.el@gmail.com>
2021-11-24 09:23:22 +0100

    Début détail recette

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,8 +1,7 @@
-
 var routes = [
   {
     path: '/',
-    url: './index.html',  
+    url: './index.html',
   },
   {
     path: '/about/',
@@ -18,6 +17,37 @@ var routes = [
   },
   {
     path: '/profile',
+    url: './pages/profile.html',
+  },
+  {
+    path: '/recipe/:recipeId',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+      // Show Preloader
+      app.preloader.show();
+
+      // Hide Preloader
+      app.preloader.hide();
+
+      // Requête GET
+      app.request.get(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${to.params.recipeId}`
+      )
+        .then(r => {
+          let recipe = JSON.parse(r.data).meals[0];
+          resolve({
+              componentUrl: "./pages/recipe-detail.html",},
+            {
+              props: {
+                recipe: recipe
+              }
+            });
+        });
+    }
+  },
+  {
+    path: '/request-and-load/user/:userId/',
     async: function ({ router, to, resolve }) {
       // App instance
       var app = router.app;
