abb83e08ae5a2b406e3240a6ddaa0ef3ec4c4f58
Florent <florent.chabin@students.campus.academy>
2021-11-25 09:13:54 +0100

    avancement des ingredients

--- www/js/store.js
+++ www/js/store.js
@@ -5,6 +5,7 @@ const store = createStore({
   state: {
     recipes: [],
     lastLetter: null,
+    ingredients: [],
   },
   getters: {
     recipes({ state }) {
@@ -12,12 +13,14 @@ const store = createStore({
     },
     lastLetter({state}){
       return state.lastLetter;
+    },
+    ingredients({ state }) {
+      return state.ingredients;
     }
   },
   actions: {
     addRecipe({ state }, recipe) {
-
-      const newRecipe = (recipe) => recipe == recipe
+      const newRecipe = (recipe) => recipe == recipe;
       const already = state.recipes.findIndex(newRecipe);
       if(already != -1){
         state.recipes = state.recipes.splice(0, already);
@@ -26,7 +29,10 @@ const store = createStore({
       }
     },
     putLastLetter({state}, letter){
-      state.lastLetter = letter
+      state.lastLetter = letter;
+    },
+    addIngredient({state}, ingredient) {
+      state.ingredients = [...state.ingredients, ingredient];
     }
   },
 })
